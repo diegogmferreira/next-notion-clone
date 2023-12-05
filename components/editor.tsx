8f91042bcaf90386b10947e8bce10c5052d09fd1
Diego Galvao <di.galvao89@gmail.com>
2023-12-04 22:48:22 -0300

    fix

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,59 +1,49 @@
-"use client";
+'use client';
 
-import {
-  BlockNoteEditor, PartialBlock
-} from "@blocknote/core";
-import "@blocknote/core/style.css";
-import {
-  BlockNoteView,
-  useBlockNote
-} from "@blocknote/react";
-import { useTheme } from "next-themes";
+import { BlockNoteEditor, PartialBlock } from '@blocknote/core';
+import '@blocknote/core/style.css';
+import { BlockNoteView, useBlockNote } from '@blocknote/react';
+import { useTheme } from 'next-themes';
 
-import { useEdgeStore } from "@/lib/edgestore";
+import { useEdgeStore } from '@/lib/edgestore';
 
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
   editable?: boolean;
-};
+}
 
-const Editor = ({
-  onChange,
-  initialContent,
-  editable
-}: EditorProps) => {
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
   const { edgestore } = useEdgeStore();
 
   const handleUpload = async (file: File) => {
-    const response = await edgestore.publicFiles.upload({ 
+    const response = await edgestore.publicFiles.upload({
       file
     });
 
     return response.url;
-  }
+  };
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
-    initialContent: 
-      initialContent 
-      ? JSON.parse(initialContent) as PartialBlock[] 
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
       : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
     uploadFile: handleUpload
-  })
+  });
 
   return (
     <div>
       <BlockNoteView
         editor={editor}
-        theme={resolvedTheme === "dark" ? "dark" : "light"}
+        theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Editor;
